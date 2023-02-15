337ab2b0c2a1f382eb66bfd9e138a0942cba8678
Maciek Palmowski <m.palmowski@freshpixels.pl>
2023-02-15 19:04:51 +0100

    Next deployment

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    public function home(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
